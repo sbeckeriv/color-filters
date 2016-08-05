4189ee1ad758e45cab5c3c2b180d29ee089a5143
Stephen Becker IV <github@deathbyescalator.com>
2016-08-05 08:03:16 -0700

    update runner

--- pixels/main.js
+++ pixels/main.js
@@ -1,21 +1,19 @@
 var child_process = require('child_process');
 
 exports.handler = function(event, context) {
-    // event is the JSON we provide to our lambda function. More on this later.
-    console.log(event["imageUrl"]);
-var env = Object.create( process.env );
-env.AWS_ACCESS_KEY_ID="";
-env.AWS_SECRET_ACCESS_KEY="";
-		// spawn a child process with our email-checker-linux binary and the event["email"] value for our argument.
-    var proc = child_process.spawn('./image', [ event["imageUrl"] ], {env:env, stdio: 'inherit' });
+  console.log(event["imageUrl"]);
+  var env = Object.create( process.env );
+  env.AWS_ACCESS_KEY_ID="";
+  env.AWS_SECRET_ACCESS_KEY="";
 
-    proc.on('close', function(code) {
-        if(code !== 0) {
-            return context.done(new Error("Process exited with non-zero status code"));
-        }
-
-        context.done(null);
-    });
+  child_process.exec('./image "'+event["imageUrl"]+'"', {env:env}, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
 
 
