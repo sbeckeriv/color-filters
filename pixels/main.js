53a3f75d60bbf1e8410f98f01be920a783ac2945
Stephen Becker IV <github@deathbyescalator.com>
2016-08-14 20:01:09 -0700

    return values

--- pixels/main.js
+++ pixels/main.js
@@ -1,25 +1,25 @@
 var child_process = require('child_process');
 
 exports.handler = function(event, context) {
-  console.log(event["imageUrl"]);
-  var env ={};
-env.LD_LIBRARY_PATH=process.env["LD_LIBRARY_PATH"];
 
-env.PATH = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
-env.AWS_ACCESS_KEY_ID="";
-env.AWS_SECRET_ACCESS_KEY="";
-env.RUST_BACKTRACE=1;
+  var env ={};
+  env.LD_LIBRARY_PATH=process.env["LD_LIBRARY_PATH"];
+
+  env.PATH = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
+  env.AWS_ACCESS_KEY_ID="";
+  env.AWS_SECRET_ACCESS_KEY="";
+  env.RUST_BACKTRACE=1;
   child_process.exec(
-'./image "'+event["imageUrl"]+'"',
-{env:env},
-(error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
+    './image "'+event["imageUrl"]+'"',
+    {env:env},
+    (error, stdout, stderr) => {
+      if (error) {
+        context.fail({message: error});
+      }else{
+        context.succeed({ "images": stdout.replace(/"/g,"").split("\n")});
+      }
       return;
-    }
-    return stdout;
-    console.log(stderr);
-  });
+    });
 }
 
 
--- pixels/src/main.rs
+++ pixels/src/main.rs
@@ -83,7 +83,7 @@ fn main() {
         request.metadata = Some(met);
         request.acl = Some(s3::CannedAcl::PublicRead);
         s3.put_object(&request);
-println!("{:?}",out_file);
+        println!("{:?}", clean_name);
     });
     std::fs::remove_file(&file);
 }
